d91cefc78a06581e3b9af10e0f3506659d561908
Zeel Patel <zeel0725@gmail.com>
2025-09-12 15:15:01 +0530

    likes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
+import ContentRating from "./Components/ContentRating"
 
 class EmployeeDetails extends React.Component{
   constructor(props) {
@@ -14,25 +15,30 @@ render() {
   const { Name, emp_id, emp_email } = this.state;
   const {emp_desg} = this.props;
     return(
-       <div className="EmployeeDetails">
+        <>
+          <div className="EmployeeDetails">
 
-        <input type="text" name="Name" value={Name}
-          onChange= {this.handleInputChange} placeholder='Enter Name'/>
-        < br />
+            <input type="text" name="Name" value={Name}
+              onChange= {this.handleInputChange} placeholder='Enter Name'/>
+            < br />
 
-        <input type="text" name="emp_id" value={emp_id}
-          onChange= {this.handleInputChange} placeholder='Enter Employee Id'/>
-        < br />
+            <input type="text" name="emp_id" value={emp_id}
+              onChange= {this.handleInputChange} placeholder='Enter Employee Id'/>
+            < br />
 
-        <input type="email" name="emp_email" value={emp_email}
-          onChange={ this.handleInputChange }
-          placeholder='Enter Employee Email' />
-        < br />
+            <input type="email" name="emp_email" value={emp_email}
+              onChange={ this.handleInputChange }
+              placeholder='Enter Employee Email' />
+            < br />
 
-        <button onClick={() => alert(`Name: ${Name}\nEmployee ID: ${emp_id}\nEmail: ${emp_email}\nEmp_desg: ${emp_desg}`)}>
-          Show Details
-        </button>
-      </div>
+            <button onClick={() => alert(`Name: ${Name}\nEmployee ID: ${emp_id}\nEmail: ${emp_email}\nEmp_desg: ${emp_desg}`)}>
+              Show Details
+            </button>
+
+          </div>
+          <ContentRating/>
+        </>
+      
     );
   }
 }export default EmployeeDetails;
